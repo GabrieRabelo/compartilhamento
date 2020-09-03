f3ea91abb570895ecd096f55f0f2de97a71389ad
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-09-03 18:46:56 -0300

    Adicioandas funcionalidades de login / logout com Spring Security.

--- src/main/java/br/pucrs/ages/townsq/model/User.java
+++ src/main/java/br/pucrs/ages/townsq/model/User.java
@@ -1,15 +1,19 @@
 package br.pucrs.ages.townsq.model;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class User {
+@Builder
+public class User implements UserDetails {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +26,34 @@ public class User {
     private String password;
     @Column(name = "bio")
     private String bio;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/br/pucrs/ages/townsq/service/UserService.java
+++ src/main/java/br/pucrs/ages/townsq/service/UserService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService implements IUserService{
+public class UserService {
 
     private final UserRepository repository;
     private final BCryptPasswordEncoder bcPasswordEncoder;
