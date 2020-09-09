c0362d19db0e2dec2354ebeeec0c75ca29c119bd
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-09-09 16:58:17 -0300

    Fix: arquivos estáticos sem autenticação.
    Correção para obter acesso aos arquivos estáticos (css, js) mesmo sem realizar login no sistema.

--- src/main/java/br/pucrs/ages/townsq/config/SecurityConfig.java
+++ src/main/java/br/pucrs/ages/townsq/config/SecurityConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final Authentication authentication;
@@ -27,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/signup/**", "/signin/**", "/", "/question").permitAll()
+                .antMatchers("/signup/**", "/signin/**", "/css/**", "/", "/question").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
