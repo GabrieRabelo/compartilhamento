cda4333b71d2c2edaecb3a48b1f59a7a21021e30
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-09-19 15:55:12 -0300

    Feat: JavaDoc.

--- src/main/java/br/pucrs/ages/townsq/controller/QuestionController.java
+++ src/main/java/br/pucrs/ages/townsq/controller/QuestionController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class QuestionController {
 
+    /**
+     * Returns the Question page with it's contents. Will be implemented on future sprints.
+     * @return question page
+     */
     @GetMapping("/question")
     public String getQuestionPage(){
         return "question";
--- src/test/java/br/pucrs/ages/townsq/UserAuthTest.java
+++ src/test/java/br/pucrs/ages/townsq/UserAuthTest.java
@@ -17,22 +17,38 @@ public class UserAuthTest {
     @Autowired
     private MockMvc mockMvc;
 
+    /**
+     * Tests the request to get the homepage of the application.
+     * @throws Exception
+     */
     @Test
     public void testGetIndexPage() throws Exception {
         this.mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
     }
 
+    /**
+     * Tests a private route (only accessible if logged in) as an anonymous user.
+     * @throws Exception
+     */
     @Test
     public void testGetUsersPageAsAnon() throws Exception {
         this.mockMvc.perform(get("/users")).andExpect(status().is3xxRedirection()).andExpect(redirectedUrl("http://localhost/signin"));
     }
 
+    /**
+     * Tests a private route (only accessible if logged in) as an logged in user.
+     * @throws Exception
+     */
     @Test
     @WithMockUser
     public void testGetUsersPageAsUser() throws Exception {
         this.mockMvc.perform(get("/users")).andExpect(status().isOk()).andExpect(view().name("users"));
     }
 
+    /**
+     * Tests the request to get the question page.
+     * @throws Exception
+     */
     @Test
     public void testGetQuestionPage() throws Exception {
         this.mockMvc.perform(get("/question")).andExpect(status().isOk()).andExpect(view().name("question"));
