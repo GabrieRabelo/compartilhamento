2d74c7ee73342e455e79b639086c3eb92682e58f
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-09-19 22:52:20 -0300

    Fix: perfil de testes.

--- src/test/java/br/pucrs/ages/townsq/UserAuthTest.java
+++ src/test/java/br/pucrs/ages/townsq/UserAuthTest.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserAuthTest {
 
     @Autowired
