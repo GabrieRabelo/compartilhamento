722d7f830e170e28c725f042bf77f6c49a3bb276
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-09-28 02:59:29 -0300

    Feat: Bruno
    Tópico e carregamento ao frontend.

--- src/main/java/br/pucrs/ages/townsq/controller/QuestionController.java
+++ src/main/java/br/pucrs/ages/townsq/controller/QuestionController.java
@@ -1,10 +1,20 @@
 package br.pucrs.ages.townsq.controller;
 
+import br.pucrs.ages.townsq.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class QuestionController {
+
+    private TopicRepository topicRepository;
+
+    @Autowired
+    public QuestionController(TopicRepository topicRepository){
+        this.topicRepository = topicRepository;
+    }
 
     /**
      * Returns the Question page with it's contents. Will be implemented on future sprints.
@@ -14,8 +24,15 @@ public class QuestionController {
     public String getQuestionPage(){
         return "question";
     }
+
+    /**
+     * Returns the question creation page with the topics from the database
+     * @param m The UI Model
+     * @return String
+     */
     @GetMapping("/question/create")
-    public String getQuestionCreatePage(){
+    public String getQuestionCreatePage(Model m){
+        m.addAttribute("topics", topicRepository.findAllByStatus(1));
         return "questionCreate";
     }
 
---
+++ src/main/java/br/pucrs/ages/townsq/model/Topic.java
@@ -0,0 +1,47 @@
+package br.pucrs.ages.townsq.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "topics")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder(toBuilder = true)
+public class Topic {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", unique = true)
+    @NotEmpty
+    @NotNull
+    private String name;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt")
+    private java.sql.Timestamp updatedAt;
+    @CreationTimestamp
+    @Column(name = "createdAt")
+    private java.sql.Timestamp createdAt;
+
+    /**
+     * Status:
+     * 1: Active
+     * 0: Inactive
+     */
+    @Column(name = "status")
+    private int status = 1;
+
+}
