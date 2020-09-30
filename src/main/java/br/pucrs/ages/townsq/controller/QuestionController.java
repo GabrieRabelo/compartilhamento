72f2ee98470aba00626b23e59ece7e8f58292899
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-09-30 15:21:10 -0300

    Fix: Bruno
    Estilos para pergunta; esqueleto para buscar pergunta por id.

--- src/main/java/br/pucrs/ages/townsq/controller/QuestionController.java
+++ src/main/java/br/pucrs/ages/townsq/controller/QuestionController.java
@@ -4,13 +4,19 @@ import br.pucrs.ages.townsq.model.Question;
 import br.pucrs.ages.townsq.model.User;
 import br.pucrs.ages.townsq.service.QuestionService;
 import br.pucrs.ages.townsq.service.TopicService;
+import br.pucrs.ages.townsq.utils.Slugify;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class QuestionController {
@@ -50,5 +56,24 @@ public class QuestionController {
         questionService.save(question);
         return "question";
     }
+
+    /*
+    @GetMapping(value = {"/question/{id}", "question/{id}/{slug}"})
+    public String getUserById(HttpServletRequest request,
+                              @PathVariable long id,
+                              @PathVariable(required = false) String slug,
+                              Model model){
+        Question question = questionService.findById(id).orElse(null);
+        if(question != null){
+            String questionSlug = Slugify.toSlug(question.getTitle());
+            if(slug == null || !slug.equals(questionSlug)){
+                request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.MOVED_PERMANENTLY);
+                return "redirect:/users/" + id + "/" + questionSlug;
+            }
+            model.addAttribute("question", question);
+            return "question";
+        }
+        return "question";
+    }*/
 
 }
