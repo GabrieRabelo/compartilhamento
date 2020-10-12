4ee6579f93067e57c11a3454127cea45ddd72b77
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-10-11 22:17:13 -0300

    Feat: @squad01.1
    Modal para cadastro de comentário.

--- src/main/java/br/pucrs/ages/townsq/controller/CommentController.java
+++ src/main/java/br/pucrs/ages/townsq/controller/CommentController.java
@@ -1,9 +1,13 @@
 package br.pucrs.ages.townsq.controller;
 
+import br.pucrs.ages.townsq.model.Comment;
 import br.pucrs.ages.townsq.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CommentController {
@@ -19,10 +23,16 @@ public class CommentController {
      * Post route to create a comment
      * @return String
      */
-    @PostMapping("/comment/create")
-    public String postCreateComment(){
-        // calls commentService.save(comment)
-        return "";
+    @PostMapping("/comment/create/{referer}/{referer_id}")
+    public String postCreateComment(@ModelAttribute Comment comment,
+                                    @PathVariable String referer,
+                                    @PathVariable long referer_id,
+                                    final RedirectAttributes redirectAttributes){
+        System.out.println(comment.getText());
+        System.out.println(referer);
+        System.out.println(referer_id);
+        redirectAttributes.addFlashAttribute("success", "Comentário cadastrado com sucesso!");
+        return "redirect:/";
     }
 
 }
