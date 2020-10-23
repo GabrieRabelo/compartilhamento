d038b645df52c8cc8ba2274f0fc77d1d5b48167a
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-10-23 17:09:55 -0300

    Feat: @squad01
    Operação de deletar e editar pergunta se possuir ROLE_MODERATOR; testes.

--- src/test/java/br/pucrs/ages/townsq/service/QuestionServiceTest.java
+++ src/test/java/br/pucrs/ages/townsq/service/QuestionServiceTest.java
@@ -1,6 +1,7 @@
 package br.pucrs.ages.townsq.service;
 
 import br.pucrs.ages.townsq.model.Question;
+import br.pucrs.ages.townsq.model.Role;
 import br.pucrs.ages.townsq.model.User;
 import br.pucrs.ages.townsq.repository.QuestionRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,12 +9,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -32,8 +30,8 @@ class QuestionServiceTest {
 	@DisplayName("Salva uma pergunta no repositorio e deve retornar a mesma")
 	@Test
 	void testSaveQuestion() {
-		Question question = new Question(null, "Olá", "essa fera ai meu", 1, new Timestamp(1), new Timestamp(1), null, null, 1, null, null);
-		User user = new User(1L, "Rabelo", "rabelo", "rabelo@rab.elo", 1, null, null, null, null, null, null, null, null, null, null);
+		Question question = Question.builder().id(1L).title("Olá").description("Essa fera aí meu").createdAt(new Timestamp(1)).updatedAt(new Timestamp(1)).build();
+		User user = User.builder().id(1L).name("Rabelo").password("rabelo").email("rabelo@rab.elo").build();
 
 		when(questionRepository.save(any(Question.class)))
 				.thenReturn(question);
@@ -48,7 +46,7 @@ class QuestionServiceTest {
 	void testGetIndexQuestions() {
 		List<Question> questionList = new ArrayList<>();
 		for(long i = 0; i<10; i++){
-			Question question = new Question(i, "Olá", "essa fera ai meu", 1, new Timestamp(i), new Timestamp(i), null, null, 1, null, null);
+			Question question = Question.builder().id(1L).title("Olá").description("Essa fera aí meu").createdAt(new Timestamp(1)).updatedAt(new Timestamp(1)).build();
 			questionList.add(question);
 		}
 
@@ -60,11 +58,11 @@ class QuestionServiceTest {
 		assertEquals(10, result.size());
 	}
 
-	@DisplayName("Deve realizar um soft delete de uma pergunta")
+	@DisplayName("Deve realizar um soft delete de uma pergunta, com o usuário criador da pergunta.")
 	@Test
 	void testDeleteQuestionOfUser() {
-		User user = new User(1L, "Rabelo", "rabelo", "rabelo@rab.elo", 1, null, null, null, null, null, null, null, null, null, null);
-		Question question = new Question(1L, "Olá", "essa fera ai meu", 1, new Timestamp(1L), new Timestamp(1L), user, null, 1, null, null);
+		User user = User.builder().id((long) 1).name("Fulano").password("123321").email("fulano@teste.com").build();
+		Question question = Question.builder().id(1L).title("Teste").description("Essa fera aí meu").user(user).createdAt(new Timestamp(1)).updatedAt(new Timestamp(1)).status(1).build();
 
 		when(questionRepository.findById(1L))
 				.thenReturn(Optional.of(question));
@@ -72,8 +70,44 @@ class QuestionServiceTest {
 		when(questionRepository.save(question))
 				.thenReturn(question);
 
-		boolean deleted = questionService.delete(1L, 1L);
+		assertTrue(questionService.delete(user, 1L));
+	}
 
-		assertTrue(deleted);
+	@DisplayName("Deve bloquear a deleção da pergunta se não for criador da pergunta e não for moderador.")
+	@Test
+	void testDeleteQuestionAsInvalidUser() {
+		User user = User.builder().id((long) 1).name("Juca").password("123321").email("juca@email.com").roles(new HashSet<>()).build();
+		User illegal = User.builder().id((long) 2).name("Illegal").password("332211").email("illegal@email.com").roles(new HashSet<>()).build();
+		Question question = Question.builder().id(1L).title("Teste").description("Essa fera aí meu").user(user).createdAt(new Timestamp(1)).updatedAt(new Timestamp(1)).status(1).build();
+
+		when(questionRepository.findById(1L))
+				.thenReturn(Optional.of(question));
+
+		when(questionRepository.save(question))
+				.thenReturn(question);
+
+		assertFalse(questionService.delete(illegal, 1L));
+	}
+
+	@DisplayName("Deve permitir a deleção da pergunta se não for criador da pergunta e for moderador.")
+	@Test
+	void testDeleteQuestionAsMod() {
+		User user = User.builder().id((long) 1).name("Juca").password("123321").email("juca@email.com").roles(new HashSet<>()).build();
+		User mod = User.builder().
+				id((long) 2).
+				name("Mod").
+				password("332211").
+				email("mod@email.com").
+				roles(new HashSet<>(Collections.singletonList(new Role(1L, "ROLE_MODERATOR")))).build();
+
+		Question question = Question.builder().id(1L).title("Teste").description("Essa fera aí meu").user(user).createdAt(new Timestamp(1)).updatedAt(new Timestamp(1)).status(1).build();
+
+		when(questionRepository.findById(1L))
+				.thenReturn(Optional.of(question));
+
+		when(questionRepository.save(question))
+				.thenReturn(question);
+
+		assertTrue(questionService.delete(mod, 1L));
 	}
 }
