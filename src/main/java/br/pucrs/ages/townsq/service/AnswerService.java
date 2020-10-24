caa4048fc74309afba2d3c5d1722411085489388
Lucas Schell <lucas.schell@acad.pucrs.br>
2020-10-24 18:52:50 -0300

    feature: Operação de deletar e editar resposta se possuir ROLE_MODERATOR

--- src/main/java/br/pucrs/ages/townsq/service/AnswerService.java
+++ src/main/java/br/pucrs/ages/townsq/service/AnswerService.java
@@ -38,7 +38,8 @@ public class AnswerService {
                                User user,
                                Long id){
         Answer databaseAnswer = answerRepository.findById(id).orElse(null);
-        if(StringUtils.isEmpty(answer.trim()) || databaseAnswer == null || !databaseAnswer.getUser().getId().equals(user.getId())){
+        if(StringUtils.isEmpty(answer.trim()) || databaseAnswer == null || (!databaseAnswer.getUser().getId().equals(user.getId()) &&
+                !user.getAuthorities().stream().anyMatch(e -> e.getAuthority().equals("ROLE_MODERATOR")))){
             throw new IllegalArgumentException("Não foi possível editar a resposta.");
         }
         databaseAnswer.setText(answer);
@@ -50,14 +51,15 @@ public class AnswerService {
 
     /**
      * Performs a soft delete of a answer if the user is it's creator
-     * @param userId
+     * @param user User
      * @param answerId
      * @return boolean
      */
-    public boolean delete(long userId, long answerId) {
+    public boolean delete(User user, long answerId) {
         Answer answer = answerRepository.findById(answerId).orElse(null);
         if(answer != null) {
-            if(answer.getUser().getId() == userId && answer.getIsActive() == 1) {
+            if(answer.getIsActive() == 1 && (answer.getUser().getId().equals(user.getId()) ||
+                    user.getAuthorities().stream().anyMatch(e -> e.getAuthority().equals("ROLE_MODERATOR")))) {
                 answer.setIsActive(0);
                 answerRepository.save(answer);
                 return true;
