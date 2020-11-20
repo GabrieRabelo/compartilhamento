34a605179b3c116b5c7083f4afc447631787afc3
lucas-g-cardoso <lucas.cardoso@pucrs.br>
2020-11-20 19:03:36 -0300

    17/Feature: @Squad1 Início código img upvote e downvote

--- src/main/java/br/pucrs/ages/townsq/model/Question.java
+++ src/main/java/br/pucrs/ages/townsq/model/Question.java
@@ -73,6 +73,18 @@ public class Question {
         return comments.stream().filter(e -> e.getIsActive() == 1).collect(Collectors.toList());
     }
 
+    public String getVoted(User user, String type){
+        VoteLog vote = votes.stream().filter(e -> e.getUser().getId().equals(user.getId())).findFirst().orElse(null);
+        if(vote == null){
+            return type + ".svg";
+        }else{
+            if(vote.getEventType().equals(type.toUpperCase()))
+                return type + "ed.svg";
+            else
+                return type + ".svg";
+        }
+    }
+
     public List<Answer> getAllActiveAnswers(){
         return answers.stream().filter(e -> e.getIsActive() == 1).collect(Collectors.toList());
     }
