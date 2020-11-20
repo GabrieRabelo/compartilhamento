00a8f6071acdbebc7079688cfa3af4df38253f8a
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-11-20 17:54:44 -0300

    Feat: @squad01
    Uso do fetch para melhor legibilidade.

--- src/main/resources/static/js/vote.js
+++ src/main/resources/static/js/vote.js
@@ -2,22 +2,15 @@ function doVote(event, target, id){
 
     const _csrf = document.getElementsByTagName("meta").namedItem('_csrf').getAttribute('content');
     const _csrf_header = document.getElementsByTagName("meta").namedItem('_csrf_header').getAttribute('content');
-    let xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
 
-    xhr.onreadystatechange = function (){
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                updateFrontend(xhr.response);
-            } else {
-                console.log("NOPE");
-            }
-        }
-    }
-
-    xhr.open('PUT', `/${event}/${target}/${id}`, true);
-    xhr.setRequestHeader(_csrf_header, _csrf);
-    xhr.send();
+    fetch(`/${event}/${target}/${id}`,{
+        headers: {
+            [_csrf_header]: _csrf
+        },
+        method: 'PUT'
+    })
+        .then(response => response.json().then(json => updateFrontend(json)))
+        .catch(err => console.log(err))
 }
 
 function updateFrontend(data){
