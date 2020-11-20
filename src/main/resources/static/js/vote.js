1691fcd3829a4f82a40b148018ae03d4532dc473
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-11-20 18:46:12 -0300

    Fix: @squad01
    Documentação e nomenclatura. Toasts.

--- src/main/resources/static/js/vote.js
+++ src/main/resources/static/js/vote.js
@@ -1,21 +1,31 @@
+/**
+ * Performs a fetch PUT to execute a vote action
+ * @param event upvote or downvote
+ * @param target question or answer
+ * @param id target id
+ */
 function doVote(event, target, id){
 
     const _csrf = document.getElementsByTagName("meta").namedItem('_csrf').getAttribute('content');
     const _csrf_header = document.getElementsByTagName("meta").namedItem('_csrf_header').getAttribute('content');
 
     fetch(`/${event}/${target}/${id}`,{
-        headers: {
-            [_csrf_header]: _csrf
-        },
-        method: 'PUT'
-    })
-        .then(response => response.json().then(json => updateFrontend(json)))
+        headers: { [_csrf_header]: _csrf },
+        method: 'PUT'}).then(response => {
+            if(response.ok) response.json().then(json => updateFrontend(json));
+            else showCustomToast('error', 'Não foi possível realizar essa ação.')
+        })
         .catch(err => console.log(err))
 }
 
+/**
+ * Updates the frontend if doVote was successful
+ * @param data Response JSON
+ */
 function updateFrontend(data){
     let element = document.querySelector(`#${data.target}-${data.id}-score`);
     const current = parseInt(element.innerHTML);
     element.innerHTML = ''
     element.innerHTML = (current + data.delta);
+    showCustomToast('success', 'Voto registrado com sucesso!');
 }
